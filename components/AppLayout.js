58d00676238e805562a64ebff6419a8c00f8aa21
조민혁 <chohyuk0725@gmail.com>
2021-12-22 19:00:01 +0900

    로그인폼작성

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
 import Link from "next/link";
 import PropTypes from "prop-types";
-import { Menu } from "antd";
+import { Menu, Input, Row, Col } from "antd";
+import UserProfile from "./UserProfile";
+import LoginForm from "./LoginForm";
+import styled from "styled-components";
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
+
+//useCallback - 함수를 캐싱
+//useMemo - 값을 캐싱
 const AppLayout = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState("");
+  const style = useMemo(() => ({ marginTop: 10 }));
+
   return (
     <div>
       {/*
@@ -23,13 +36,29 @@ const AppLayout = ({ children }) => {
             </Link>
           </Menu.Item>
           <Menu.Item>
-            <Link href="/signup">
-              <a>회원가입</a>
-            </Link>
+            <SearchInput style={{ verticalAlign: "middle" }}></SearchInput>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/signup">회원가입</Link>
           </Menu.Item>
         </Menu>
       </div>
-      {children}
+      <div style={style}>
+        <Row gutter={8}>
+          <Col xs={24} md={6}>
+            {isLoggedIn ? <UserProfile /> : <LoginForm />}
+          </Col>
+          <Col xs={24} md={12}>
+            {children}
+          </Col>
+          <Col xs={24} md={6}>
+            오른쪽 메뉴
+            <a href="www.naver.com" target="_blank" rel="noreferrer noopener">
+              Made by Aiden
+            </a>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 };
---
+++ components/LoginForm.js
@@ -0,0 +1,56 @@
+import React, { useState, useCallback } from "react";
+import { Form, Input, Button } from "antd";
+import Link from "next/link";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  margin-top: 10px;
+`;
+const LoginForm = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const onChangeId = useCallback((e) => {
+    setId(e.target.value);
+  }, []);
+  //   컴포넌트에 프롭스로 넘겨주는 함수는 유즈콜백 사용하기 (최적화 관련)
+  const onChangePassword = useCallback((e) => {
+    setPassword(e.target.value);
+  }, []);
+  // 컴포넌트에 프롭스로 넘겨주는 함수는 유즈콜백 사용하기 (최적화 관련)
+
+  // 커스텀 훅으로 변경 여지 있음,
+  return (
+    <Form>
+      <div>
+        <label htmlFor="user-id"></label>
+        <br />
+        <Input name="user-id" value={id} onChange={onChangeId} required />
+      </div>
+      <div>
+        <label htmlFor="user-password"></label>
+        <br />
+        <Input
+          name="user-password"
+          type="password"
+          value={password}
+          onChange={onChangePassword}
+          required
+        />
+      </div>
+      <ButtonWrapper>
+        <button style={{ marginTop: "10px" }}>noo</button>
+        {/* 해당 인라인 스타일은 매번 랜더싱을 다시할 수 있음 */}
+        <Button tupe="Primary" htmlType="submit" loading={false}>
+          로그인
+        </Button>
+        <Link href="/signup">
+          <a>
+            <Button>회원가입</Button>
+          </a>
+        </Link>
+      </ButtonWrapper>
+    </Form>
+  );
+};
+
+export default LoginForm;
